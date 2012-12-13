4dda87151c1feba5a8acad844b5653d3ae9c23d5
Peter Graves <gnooth@gmail.com>
2012-12-12 20:28:29 -0800

    minor cleanup

--- execute.asm
+++ execute.asm
@@ -13,7 +13,8 @@
 ; You should have received a copy of the GNU General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-code execute, 'execute'
+code execute, 'execute'                 ; i*x xt -- j*x
+; CORE
         mov     rax, [rbx]
         poprbx
         call    rax
