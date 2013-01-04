6de91288ce5ef97957d5a8d7de091f8b70f0273d
Peter Graves <gnooth@gmail.com>
2013-01-04 12:48:49 -0800

    search-order word set and search-order extensions

--- find.asm
+++ find.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2012 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2012-2013 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -13,11 +13,175 @@
 ; You should have received a copy of the GNU General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-code match?, 'match?'                   ; c-addr1 c-addr2 -- flag
-        _ count
-        _ rot
-        _ count
-        _ istrequal
+variable current, 'current', forth_wordlist_data
+
+code nvocs, '#vocs'
+        pushrbx
+        mov     rbx, NVOCS
+        next
+endcode
+
+variable norder, '#order', 1
+
+variable context, 'context', forth_wordlist_data
+section .data
+        times NVOCS dq 0
+        dq      0                       ; sentinel for FIND
+
+code get_current, 'get-current'         ; -- wid
+        pushrbx
+        mov     rbx, [current_data]
+        next
+endcode
+
+code set_current, 'set-current'         ; wid --
+        mov     [current_data], rbx
+        poprbx
+        next
+endcode
+
+code definitions, 'definitions'         ; --
+        mov     eax, [context_data]
+        mov     [current_data], eax
+        next
+endcode
+
+code wordlist, 'wordlist'               ; -- wid
+; SEARCH
+        _ here
+        _ zero
+        _ comma                         ; link
+        next
+endcode
+
+section .data
+forth_wordlist_data:
+        dq      0
+
+code forth_wordlist, 'forth-wordlist'   ; -- wid
+; SEARCH
+        pushrbx
+        mov     rbx, forth_wordlist_data
+        next
+endcode
+
+code forth, 'forth'
+        mov     rax, forth_wordlist_data
+        mov     [context_data], rax
+        next
+endcode
+
+code order, 'order'
+; SEARCH EXT
+; FIXME
+        _ nvocs
+        _ zero
+        _do order1
+        _ context
+        _ i
+        _cells
+        _ plus
+        _fetch
+        _ udot
+        _loop order1
+        next
+endcode
+
+code get_order, 'get-order'             ; -- widn ... wid1 n
+; SEARCH
+; "wid1 identifies the word list that is searched first, and widn the word list
+; that is searched last."
+        _ norder
+        _fetch
+        _ zero
+        _?do get_order1
+        _ norder
+        _fetch
+        _ i
+        _ minus
+        _oneminus
+        _cells
+        _ context
+        _ plus
+        _ fetch
+        _loop get_order1
+        _ norder
+        _ fetch
+        next
+endcode
+
+code set_order, 'set-order'             ; widn ... wid1 n --
+; SEARCH
+; "Set the search order to the word lists identified by widn ... wid1.
+; Subsequently, word list wid1 will be searched first, and word list widn
+; searched last."
+        mov     rax, rbx
+        poprbx
+        test    rax, rax
+        jnz     .1
+        ; "If n is zero, empty the search order."
+        _ context
+        _ nvocs
+        _cells
+        _ erase
+        _ zero
+        _ norder
+        _ store
+        _return
+.1:
+        cmp     rax, -1
+        jnz     .2
+        ; "If n is minus one, set the search order to the implementation-
+        ; defined minimum search order."
+        _ only
+        _return
+.2:
+        pushrbx
+        mov     rbx, rax
+        _ context
+        _ nvocs
+        _cells
+        _ erase
+        _ dup
+        _ norder
+        _ store
+        _ zero
+        _?do set_order1
+        _ context
+        _ i
+        _cells
+        _ plus
+        _ store
+        _loop set_order1
+        next
+endcode
+
+code also, 'also'
+        _ get_order
+        _ over
+        _ swap
+        _ oneplus
+        _ set_order
+        next
+endcode
+
+code only, 'only'
+        _ context
+        _ nvocs
+        _cells
+        _ erase
+        _ forth                         ; FIXME
+        _ one
+        _ norder
+        _ store
+        next
+endcode
+
+code previous, 'previous'
+        _ get_order
+        _ nip
+        _ oneminus
+        _ set_order
         next
 endcode
 
@@ -33,61 +197,77 @@ code found, 'found'                     ; nfa -- xt 1  | xt -1
         next
 endcode
 
-variable current, 'current', forth_wordlist_data
-
-variable context, 'context', forth_wordlist_data
-
-section .data
-forth_wordlist_data:
-        dq      0
-
-code forth_wordlist, 'forth-wordlist'   ; -- wid
-; SEARCH
-        pushrbx
-        mov     rbx, forth_wordlist_data
-        next
-endcode
-
 code search_wordlist, 'search-wordlist' ; c-addr u wid -- 0 | xt 1 | xt -1
 ; SEARCH
         _ fetch                         ; last link in wordlist
-        _begin sw1                      ; -- c-addr u nfa
+        _ dup
+        _if sw1
+        _begin sw2                      ; -- c-addr u nfa
         _duptor                         ; -- c-addr u nfa                       r: -- nfa
         _ count                         ; -- c-addr u c-addr' u'                r: -- nfa
         _ twoover                       ; -- c-addr u c-addr' u' c-addr-u       r: -- nfa
         _ istrequal                     ; -- c-addr u flag                      r: -- nfa
-        _if sw2                         ; -- c-addr u                           r: -- nfa
+        _if sw3                         ; -- c-addr u                           r: -- nfa
         ; found it!
         _ twodrop                       ; --                                    r: -- nfa
         _ rfrom                         ; -- nfa
        _ found                         ; -- xt 1 | xt -1
         _return
-        _then sw2                       ; -- c-addr u                           r: -- nfa
+        _then sw3                       ; -- c-addr u                           r: -- nfa
         _ rfrom                         ; -- c-addr u nfa
         _ ntolink                       ; -- c-addr u lfa
         _ fetch                         ; -- c-addr u nfa
         _ dup                           ; -- c-addr u nfa nfa
         _ zero?
-        _until sw1
+        _until sw2
+        _then sw1
         _ threedrop
         _ false
         next
 endcode
 
+section .data
+find_arg:       dq      0
+find_addr:      dq      0
+find_len:       dq      0
+
 code find, 'find'                       ; c-addr -- c-addr 0  |  xt 1  |  xt -1
 ; CORE, SEARCH
-        _duptor
-        _ count
+        mov     [find_arg], rbx
+        _ count                         ; -- addr len
+        mov     [find_len], rbx
+        poprbx
+        mov     [find_addr], rbx
+        poprbx                          ; --
+        _ nvocs
+        _ zero
+        _do find1
         _ context
-        _ fetch
-        _ search_wordlist
+        _ i
+        _cells
+        _ plus
+        _ fetch                         ; -- wid
         _ dup
-        _if find1
-        _rfromdrop
-        _else find1
-        _ rfrom
+        _ zero?
+        _if find2                       ; not found
+        pushrbx
+        mov     rbx, [find_arg]
         _ swap
-        _then find1
+        _ unloop
+        _return
+        _then find2                     ; -- wid
+        pushrbx
+        mov     rbx, [find_addr]
+        pushrbx
+        mov     rbx, [find_len]         ; -- wid addr len
+        _ rot
+        _ search_wordlist
+        _ ?dup
+        _if find3                       ; -- xt n
+        _ unloop
+        _return
+        _then find3
+        _loop find1
         next
 endcode
 
