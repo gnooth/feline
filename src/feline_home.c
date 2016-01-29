29b93a24d8cea99449ef5e2cdb147e1e22cea9de
Peter Graves <gnooth@gmail.com>
2016-01-28 16:12:13 -0800

    feline_home.c: FELINE_SOURCE_DIR

--- src/feline_home.c
+++ src/feline_home.c
@@ -3,6 +3,11 @@
 
 int main()
 {
+#ifdef WIN64
+  char sep = '\\';
+#else
+  char sep = '/';
+#endif
   char buf[1024];
   FILE *file;
   int i = 0;
@@ -12,6 +17,7 @@ int main()
   if (file)
     {
       fprintf(file, "%%define FELINE_HOME \"%s\"\n", buf);
+      fprintf(file, "%%define FELINE_SOURCE_DIR \"%s%csrc\"\n", buf, sep);
       fclose(file);
     }
 }
