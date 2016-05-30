b2306284422ac0dd99a4ddf74444fa28a8abaa0d
Peter Graves <gnooth@gmail.com>
2016-05-30 11:44:25 -0700

    * src/sequences.asm (two_nth_unsafe): Use nth_unsafe, not nth.

--- src/sequences.asm
+++ src/sequences.asm
@@ -36,7 +36,7 @@ endcode
 code two_nth_unsafe, '2nth-unsafe'      ; n seq1 seq2 -- elt1 elt2
         _tor                            ; -- n seq1     r: -- seq2
         _dupd                           ; -- n n seq1   r: -- seq2
-        _ nth                           ; -- n elt1     r: -- seq2
+        _ nth_unsafe                    ; -- n elt1     r: -- seq2
         _swap                           ; -- elt1 n     r: -- seq2
         _rfrom                          ; -- elt1 n seq2
         _ nth                           ; -- elt2 elt2
