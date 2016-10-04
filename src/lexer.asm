d298edd2cd67216aac9c22c0ef739a590c17a5f8
Peter Graves <gnooth@gmail.com>
2016-10-04 11:55:54 -0700

    * src/object-macros.asm (OBJECT_TYPE_LEXER): New object type. * src/lexer.asm: New file. * src/forth.asm: Add lexer.asm. * src/Makefile: Add lexer.asm.

---
+++ src/lexer.asm
@@ -0,0 +1,257 @@
+; Copyright (C) 2016 Peter Graves <gnooth@gmail.com>
+
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+file __FILE__
+
+%macro  _lexer_string 0                 ; lexer -- string
+        _slot1
+%endmacro
+
+%macro  _this_lexer_string 0            ; -- string
+        _this_slot1
+%endmacro
+
+%macro  _this_lexer_set_string 0        ; string --
+        _this_set_slot1
+%endmacro
+
+%macro  _lexer_index 0                  ; lexer -- index
+        _slot2
+%endmacro
+
+%macro  _this_lexer_index 0             ; -- index
+        _this_slot2
+%endmacro
+
+%macro  _lexer_set_index 0              ; index lexer --
+        _set_slot2
+%endmacro
+
+%macro  _this_lexer_set_index 0         ; index --
+        _this_set_slot2
+%endmacro
+
+; ### lexer?
+code lexer?, 'lexer?'                   ; handle -- ?
+        _dup
+        _ handle?
+        _if .1
+        _handle_to_object_unsafe        ; -- object
+        _dup_if .2
+        _object_type                    ; -- object-type
+        _eq?_literal OBJECT_TYPE_LEXER
+        _return
+        _then .2
+        _then .1
+        mov     ebx, f_value
+        next
+endcode
+
+; ### error-not-lexer
+code error_not_lexer, 'error-not-lexer' ; x --
+        ; REVIEW
+        _error "not a lexer"
+        next
+endcode
+
+; ### verify-lexer
+code verify_lexer, 'verify-lexer'       ; handle -- handle
+        _dup
+        _ lexer?
+        _tagged_if .1
+        _return
+        _then .1
+
+        _ error_not_lexer
+        next
+endcode
+
+; ### check-lexer
+code check_lexer, 'check-lexer'         ; x -- lexer
+        _dup
+        _ handle?
+        _if .1
+        _handle_to_object_unsafe        ; -- object/0
+        _dup_if .2
+        _dup
+        _object_type                    ; -- object object-type
+        _eq?_literal OBJECT_TYPE_LEXER
+        _tagged_if .3
+        _return
+        _then .3
+        _then .2
+        _then .1
+
+        _ error_not_lexer
+        next
+endcode
+
+; ### lexer-string
+code lexer_string, 'lexer-string'       ; lexer -- string
+        _ check_lexer
+        _lexer_string
+        next
+endcode
+
+; ### lexer-index
+code lexer_index, 'lexer-index'         ; lexer -- index
+        _ check_lexer
+        _lexer_index
+        _tag_fixnum
+        next
+endcode
+
+; ### <lexer>
+code lexer, '<lexer>'                   ; string -- lexer
+; 3 cells: object header, string, index
+
+        _lit 3
+        _ allocate_cells
+
+        push    this_register
+        mov     this_register, rbx
+        poprbx                          ; -- object callable
+
+        _this_object_set_type OBJECT_TYPE_LEXER
+
+        _ verify_string
+        _this_lexer_set_string
+
+        pushrbx
+        mov     rbx, this_register      ; -- lexer
+
+        ; Return handle.
+        _ new_handle                    ; -- handle
+
+        pop     this_register
+        next
+endcode
+
+; : skip-blank ( )
+;     lexer-index lexer-string string-skip-whitespace ( -- index/f )
+;     [ lexer-index! ] [ lexer-string length lexer-index! ] if* ;
+
+; ### skip-blank
+code skip_blank, 'skip-blank'           ; lexer --
+        _ check_lexer
+
+        push    this_register
+        mov     this_register, rbx
+        poprbx
+
+        _this_lexer_index
+        _tag_fixnum
+        _this_lexer_string
+        _ string_skip_whitespace        ; -- index/f
+
+        _dup
+        _tagged_if .1
+        _untag_fixnum
+        _this_lexer_set_index
+        _else .1
+        _drop
+        _this_lexer_string
+        _ string_length
+        _untag_fixnum
+        _this_lexer_set_index
+        _then .1
+
+        pop     this_register
+        next
+endcode
+
+; : skip-word ( )
+;     lexer-index lexer-string string-skip-to-whitespace ( -- index/f )
+;     [ lexer-index! ] [ lexer-string length lexer-index! ] if* ;
+
+; ### skip-word
+code skip_word, 'skip-word'             ; lexer --
+        _ check_lexer
+
+        push    this_register
+        mov     this_register, rbx
+        poprbx
+
+        _this_lexer_index
+        _tag_fixnum
+        _this_lexer_string
+        _ string_skip_to_whitespace     ; -- index/f
+
+        _dup
+        _tagged_if .1
+        _untag_fixnum
+        _this_lexer_set_index
+        _else .1
+        _drop
+        _this_lexer_string
+        _ string_length
+        _untag_fixnum
+        _this_lexer_set_index
+        _then .1
+
+        pop     this_register
+        next
+endcode
+
+; ### .lexer
+code dot_lexer, '.lexer'                ; lexer --
+        _ check_lexer
+
+        push    this_register
+        mov     this_register, rbx
+        poprbx
+
+        _quote "LEXER{ "
+        _ write_string
+
+        _this_lexer_string
+        _ string_length
+        _lit tagged_fixnum(40)
+        _ fixnum_le
+        _tagged_if .1
+
+        _this_lexer_string
+        _ dot_object
+
+        _else .1
+
+        _lit tagged_char('"')
+        _ write_char
+
+        _this_lexer_string
+        _lit tagged_fixnum(37)
+        _ string_head
+        _ write_string
+
+        _quote "..."
+        _ write_string
+
+        _lit tagged_char('"')
+        _ write_char
+
+        _then .1
+
+        _ space
+
+        _this_lexer_index
+        _tag_fixnum
+        _ dot_object
+
+        _quote "}"
+        _ write_string
+
+        pop     this_register
+        next
+endcode
--- src/object-macros.asm
+++ src/object-macros.asm
@@ -32,6 +32,7 @@ OBJECT_TYPE_TUPLE               equ 13
 OBJECT_TYPE_CURRY               equ 14
 OBJECT_TYPE_SLICE               equ 15
 OBJECT_TYPE_RANGE               equ 16
+OBJECT_TYPE_LEXER               equ 17
 
 ; Object flag bits.
 OBJECT_MARKED_BIT               equ 1
