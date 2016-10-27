2cecf0935f900ce76ab875f1062617c446ba70aa
Peter Graves <gnooth@gmail.com>
2016-10-27 11:34:07 -0700

    * src/strings.asm: Removed dead code.

--- src/strings.asm
+++ src/strings.asm
@@ -82,135 +82,6 @@ code zcount, 'zcount'                   ; zaddr -- zaddr len
         next
 endcode
 
-; ### string,
-code stringcomma, 'string,'             ; addr u --
-; not in standard
-        _ here
-        _ over
-        _oneplus
-        _ allot
-        _ place
-        ; terminal null byte
-        _zero
-        _ ccomma
-        next
-endcode
-
-do_cliteral:
-        pushrbx
-        db      $48                     ; mov rbx, 0
-        db      $0bb
-        dq      0                       ; 64-bit immediate value (to be patched)
-do_cliteral_end:
-
-; ### cliteral
-code cliteral, 'cliteral', IMMEDIATE    ; c: addr1 u --         runtime: -- c-addr2
-; not in standard
-        _ ?comp
-        _ flush_compilation_queue
-        _ here                          ; addr for counted string
-        _ rrot                          ; -- here addr1 u
-        _ stringcomma                   ; -- here
-        _lit do_cliteral
-        _lit do_cliteral_end - do_cliteral
-        _ paren_copy_code
-        _ here_c
-        _cellminus
-        _ store
-        next
-endcode
-
-; ### sliteral-string,
-code sliteral_stringcomma, 'sliteral-string,'   ; addr u --
-; not in standard
-        _ here
-        _ over
-        _ allot
-        _ swap
-        _ move
-        ; terminal null byte
-        _zero
-        _ ccomma
-        next
-endcode
-
-do_sliteral:
-        lea     rbp, [rbp - BYTES_PER_CELL * 2]
-        mov     [rbp + BYTES_PER_CELL], rbx
-        db      $48
-        db      $0bb
-do_sliteral_end:
-
-; ### sliteral
-code sliteral, 'sliteral', IMMEDIATE    ; c: c-addr1 u --       runtime: -- c-addr2 u
-; STRING
-; "Interpretation semantics for this word are undefined."
-        _ ?comp
-        _ flush_compilation_queue
-        _ here                          ; addr for counted string
-        _ rrot                          ; -- here c-addr1 u
-        _twodup                         ; -- here c-addr1 u c-addr1 u
-        _ sliteral_stringcomma          ; -- here c-addr1 u
-        _nip                            ; -- here u
-        _swap                           ; -- u here
-
-        _lit do_sliteral
-        _lit do_sliteral_end - do_sliteral
-        _ paren_copy_code               ; -- u here
-
-        _ commac                        ; -- u
-
-        ; mov [rbp], rbx
-        _ccommac $48
-        _ccommac $89
-        _ccommac $5d
-        _ccommac 0
-
-        _ccommac $48
-        _ccommac $0bb
-        _ commac
-
-        next
-endcode
-
-; ### c"
-code cquote, 'c"', IMMEDIATE
-; CORE EXT
-; "Interpretation semantics for this word are undefined."
-        _ ?comp
-        _lit '"'
-        _ parse                         ; -- c-addr u
-        _ cliteral
-        next
-endcode
-
-; ### s"
-code squote, 's"', IMMEDIATE
-; CORE, FILE
-        _lit '"'
-        _ parse                         ; -- c-addr u
-        _ statefetch
-        _if .1
-        _ sliteral
-        _else .1
-        _ copy_to_string
-        _ string_from
-        _then .1
-        next
-endcode
-
-; ### ."
-code dotquote, '."', IMMEDIATE
-; CORE
-; "Interpretation semantics for this word are undefined."
-        _ ?comp
-        _ flush_compilation_queue
-        _ squote
-        _lit type
-        _ commacall
-        next
-endcode
-
 ; ### (abort")
 code parenabortquote, '(abort")'        ; flag c-addr --
         _swap
@@ -221,18 +92,6 @@ code parenabortquote, '(abort")'        ; flag c-addr --
         _else .1
         _drop
         _then .1
-        next
-endcode
-
-; ### abort"
-code abortquote, 'abort"', IMMEDIATE
-; CORE
-; "Interpretation semantics for this word are undefined."
-        _ ?comp
-        _ flush_compilation_queue
-        _ cquote
-        _lit parenabortquote
-        _ commacall
         next
 endcode
 
@@ -284,39 +143,28 @@ code blank, 'blank'                     ; addr u --
         next
 endcode
 
-; ### buffer:
-code buffer_colon, 'buffer:'            ; u "<spaces>name" --
-                                        ; Execution: -- a-addr
-; CORE EXT
-; "Reserve u address units at an aligned address. Contiguity of
-; this region with any other region is undefined."
-        _ create
-        _ allot
-        next
-endcode
-
-; ### compare
-code compare, 'compare'                 ; c-addr1 u1 c-addr2 u2 -- n
-; STRING
-; adapted from Win32Forth
-        mov     rdi, [rbp]                              ; c-addr2 in RDI
-        mov     rcx, [rbp + BYTES_PER_CELL]             ; u1 in RCX
-        mov     rsi, [rbp + BYTES_PER_CELL * 2]         ; c-addr1 in RSI
-        mov     rdx, 1
-        cmp     rcx, rbx                ; compare lengths
-        cmova   rcx, rbx                ; compare shorter of the strings
-        cmova   rbx, rdx                ; set string1 longer flag
-        mov     rdx, 0
-        cmove   rbx, rdx                ; strings equal lengths
-        mov     rdx, -1
-        cmovb   rbx, rdx                ; set string2 longer flag
-        repz    cmpsb                   ; compare the strings
-        mov     rsi, 1
-        cmovb   rbx, rdx                ; string1 > string2
-        cmova   rbx, rsi                ; string1 < string2
-        lea     rbp, [rbp + BYTES_PER_CELL * 3]
-        next
-endcode
+; ; ### compare
+; code compare, 'compare'                 ; c-addr1 u1 c-addr2 u2 -- n
+; ; STRING
+; ; adapted from Win32Forth
+;         mov     rdi, [rbp]                              ; c-addr2 in RDI
+;         mov     rcx, [rbp + BYTES_PER_CELL]             ; u1 in RCX
+;         mov     rsi, [rbp + BYTES_PER_CELL * 2]         ; c-addr1 in RSI
+;         mov     rdx, 1
+;         cmp     rcx, rbx                ; compare lengths
+;         cmova   rcx, rbx                ; compare shorter of the strings
+;         cmova   rbx, rdx                ; set string1 longer flag
+;         mov     rdx, 0
+;         cmove   rbx, rdx                ; strings equal lengths
+;         mov     rdx, -1
+;         cmovb   rbx, rdx                ; set string2 longer flag
+;         repz    cmpsb                   ; compare the strings
+;         mov     rsi, 1
+;         cmovb   rbx, rdx                ; string1 > string2
+;         cmova   rbx, rsi                ; string1 < string2
+;         lea     rbp, [rbp + BYTES_PER_CELL * 3]
+;         next
+; endcode
 
 ; ### mem=
 code memequal, 'mem='                   ; addr1 addr2 len -- flag
@@ -443,18 +291,3 @@ inline count, 'count'                   ; c-addr -- c-addr+1 u
 ; CORE 6.1.0980
         _count
 endinline
-
-; ### -trailing
-code dashtrailing, '-trailing'          ; c-addr u1 -- c-addr u2
-; STRING
-        test    rbx, rbx
-        jz      .2
-        mov     rax, [rbp]
-.1:
-        cmp     byte [rax + rbx - 1], ' '
-        jne     .2
-        dec     rbx
-        jnz     .1
-.2:
-        next
-endcode
