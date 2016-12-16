cd2e6347974a977d5727279668f56e65fcea5d18
Peter Graves <gnooth@gmail.com>
2016-12-15 17:06:13 -0800

    * src/bignum.c (make_bignum): New. (bignum_add): New.

--- src/bignum.c
+++ src/bignum.c
@@ -15,15 +15,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>     // memset
 
 #include "../gmp/gmp.h"
 
 #include "feline.h"
 
+#define MOST_POSITIVE_FIXNUM          1152921504606846975
+#define MOST_NEGATIVE_FIXNUM         -1152921504606846976
+
+struct bignum {
+  Cell object_header;
+  mpz_t z;
+};
+
 void *bignum_allocate()
 {
   // + 8 for object header
   return malloc(sizeof(mpz_t) + 8);
+}
+
+struct bignum *make_bignum(mpz_t z)
+{
+  struct bignum *p = bignum_allocate();
+  memset(p, 0, sizeof(struct bignum));
+  mpz_init_set(p->z, z);
+  return p;
 }
 
 void bignum_free(mpz_t z)
@@ -44,6 +61,26 @@ void bignum_init_set_ui(mpz_t z, unsigned long int n)
 void bignum_init_set_si(mpz_t z, signed long int n)
 {
   mpz_init_set_si(z, n);
+}
+
+void *bignum_add(struct bignum *b, long n)
+{
+  mpz_t result;
+  mpz_init_set(result, b->z);
+  if (n >= 0)
+    mpz_add_ui(result, result, (unsigned long) n);
+  else
+    mpz_sub_ui(result, result, (unsigned long) -n);
+  if (mpz_fits_slong_p(result))
+    {
+      long n = mpz_get_si(result);
+      if (n >= MOST_NEGATIVE_FIXNUM && n <= MOST_POSITIVE_FIXNUM)
+        {
+          mpz_clear(result);
+          return (void *) ((n << 3) + 1);
+        }
+    }
+  return make_bignum(result);
 }
 
 size_t bignum_sizeinbase(const mpz_t z, int base)
