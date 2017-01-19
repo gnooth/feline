165b6f9a756586f72a9b7b567ecfa10cb2bf5fea
Peter Graves <gnooth@gmail.com>
2017-01-18 18:15:00 -0800

    * src/load.asm (interactive_load): Change Feline name from ':l' to 'l'.

--- src/load.asm
+++ src/load.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2016 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2016-2017 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -94,8 +94,8 @@ code load_system_file, 'load-system-file' ; filename --
         next
 endcode
 
-; ### :l
-code interactive_load, ':l', SYMBOL_IMMEDIATE   ; --
+; ### l
+code interactive_load, 'l', SYMBOL_IMMEDIATE   ; --
         _ interactive?
         _ get
         _tagged_if .1
