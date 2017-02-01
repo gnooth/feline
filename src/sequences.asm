65df95edb5b00ec7756046d44067c39ea553aa86
Peter Graves <gnooth@gmail.com>
2017-02-01 09:24:59 -0800

    * src/sequences.asm (min-length): Use fixnum-min. * src/test.feline (min-length-test-1): New test.

--- src/sequences.asm
+++ src/sequences.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2016 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2016-2017 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -65,7 +65,7 @@ endcode
 code min_length, 'min-length'           ; seq1 seq2 -- n
         _lit S_length
         _ bi_at
-        _ min
+        _ fixnum_min
         next
 endcode
 
