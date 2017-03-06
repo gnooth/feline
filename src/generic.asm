ab569d26634479e7a4a6c70a98f5ac4af87762b5
Peter Graves <gnooth@gmail.com>
2017-03-06 10:49:02 -0800

    * src/generic.asm (initialize-generic-functions): Add equal? method for floats.

--- src/generic.asm
+++ src/generic.asm
@@ -151,6 +151,7 @@ code initialize_generic_functions, 'initialize-generic-functions' ; --
         _add_method equal?, OBJECT_TYPE_SYMBOL, symbol_equal?
         _add_method equal?, OBJECT_TYPE_F, f_equal?
         _add_method equal?, OBJECT_TYPE_BIGNUM, bignum_equal?
+        _add_method equal?, OBJECT_TYPE_FLOAT, float_equal?
 
         ; length
         _initialize_generic_function length
