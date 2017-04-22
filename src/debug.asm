a282be635f066ebe3ac82ca23e7aa5f7987b5b00
Peter Graves <gnooth@gmail.com>
2017-04-22 09:32:13 -0700

    * src/debug.asm (debug_enabled): Make this an asm global.

--- src/debug.asm
+++ src/debug.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2016 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2016-2017 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 
 file __FILE__
 
-_global debug_enabled, t_value
+asm_global debug_enabled, t_value
 
 ; ### debug?
 code debug?, 'debug?'                   ; -- ?
