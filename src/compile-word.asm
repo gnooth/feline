3ee44616dff47d3193995eff2fb267f3cf48331b
Peter Graves <gnooth@gmail.com>
2017-04-28 10:15:00 -0700

    * src/compile-word.asm (compile-pair): Use unchecked-call-symbol.

--- src/compile-word.asm
+++ src/compile-word.asm
@@ -329,7 +329,7 @@ code compile_pair, 'compile-pair'       ; pair --
         _then .2
 
         _ compile_literal
-        _lit call_symbol
+        _lit unchecked_call_symbol
         _ compile_call
 
         next
