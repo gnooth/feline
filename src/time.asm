97ed092b45fee5dbdfe037212dd7fa062d82efad
Peter Graves <gnooth@gmail.com>
2017-06-18 10:17:16 -0700

    * src/time.asm (rdtsc): Serialize.

--- src/time.asm
+++ src/time.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2012-2016 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2012-2017 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -19,6 +19,10 @@ file __FILE__
 
 ; ### rdtsc
 inline read_time_stamp_counter, 'rdtsc' ; -- u
+        ; serialize
+        xor     eax, eax
+        cpuid
+
         _rdtsc
 endinline
 
