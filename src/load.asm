327ebf16582fcd8c99428c32fec5780f2c1c5038
Peter Graves <gnooth@gmail.com>
2017-09-19 17:30:08 -0700

    * src/load.asm (load): Remove call to not-in-definition.

--- src/load.asm
+++ src/load.asm
@@ -194,8 +194,6 @@ code load, 'load'                       ; path --
         _lit S_do_error1        ; recover
         _ recover
 
-        _ not_in_definition
-
         _ restore_search_order
 
         _ end_scope
