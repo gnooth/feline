4e879f6ced643d375b7a966f85a52e70fa11913f
Peter Graves <gnooth@gmail.com>
2017-10-06 08:45:36 -0700

    * src/object-macros.asm (TYPECODE_TUPLE_CLASS): New. (LAST_BUILTIN_TYPECODE): Update.

--- src/object-macros.asm
+++ src/object-macros.asm
@@ -52,8 +52,9 @@ TYPECODE_METHOD                 equ 20
 TYPECODE_GENERIC_FUNCTION       equ 21
 TYPECODE_UINT64                 equ 22
 TYPECODE_INT64                  equ 23
+TYPECODE_TUPLE_CLASS            equ 24
 
-LAST_BUILTIN_TYPECODE           equ 23
+LAST_BUILTIN_TYPECODE           equ 24
 
 ; Object flag bits.
 OBJECT_MARKED_BIT               equ 1
