acd956231581188c25ed300411ee3d33cfb40590
Peter Graves <gnooth@gmail.com>
2017-10-12 16:31:24 -0700

    Remove dead code
    * src/tuple.asm (tuple?, check-tuple): Remove.
    (tuple-layout, layout-of): Remove.
    (<tuple>, new): Remove.
    (.tuple): Remove.

--- src/tuple.asm
+++ src/tuple.asm
@@ -15,48 +15,10 @@
 
 file __FILE__
 
-; ### tuple?
-code tuple?, 'tuple?'                   ; handle -- ?
-        _dup
-        _ handle?
-        _tagged_if .1
-        _handle_to_object_unsafe        ; -- object
-        _dup_if .2
-        _object_raw_typecode
-        _lit TYPECODE_TUPLE
-        _eq?
-        _return
-        _then .2
-        _then .1
-        mov     ebx, f_value
-        next
-endcode
-
 ; ### error-not-tuple
 code error_not_tuple, 'error-not-tuple' ; x --
         ; REVIEW
         _error "not a tuple"
-        next
-endcode
-
-; ### check-tuple
-code check_tuple, 'check-tuple'         ; handle -- tuple
-        _dup
-        _ handle?
-        _tagged_if .1
-        _handle_to_object_unsafe        ; -- object/0
-        _dup_if .2
-        _dup
-        _object_raw_typecode
-        _lit TYPECODE_TUPLE
-        _equal
-        _if .3
-        _return
-        _then .3
-        _then .2
-        _then .1
-
-        _ error_not_tuple
         next
 endcode
 
@@ -93,24 +55,9 @@ code check_tuple_instance, 'check_tuple_instance', SYMBOL_INTERNAL
         next
 endcode
 
-; ### tuple-layout
-code tuple_layout, 'tuple-layout'       ; class-symbol -- layout
-        _quote "layout"
-        _swap
-        _ symbol_prop
-        next
-endcode
-
 %macro _tuple_layout_of 0               ; tuple -- layout
         _slot 1
 %endmacro
-
-; ### layout-of
-code layout_of, 'layout-of'             ; tuple -- layout
-        _ check_tuple
-        _tuple_layout_of
-        next
-endcode
 
 ; ### tuple-size
 code tuple_size, 'tuple-size'           ; tuple -- size
@@ -123,50 +70,6 @@ tuple_size_unchecked:
         _tuple_layout_of
         _ array_second
 
-        next
-endcode
-
-; ### <tuple>
-code new_tuple, '<tuple>'               ; class-symbol -- handle
-
-        _ tuple_layout                  ; -- layout
-
-        _dup
-        _ second
-        _ check_fixnum
-        _dup
-        ; Slot 0 is object header, slot 1 is class.
-        add     rbx, 2
-        _ raw_allocate_cells            ; -- layout untagged-size tuple
-
-        push    this_register
-        popd    this_register           ; -- layout untagged-size
-
-        _this_object_set_raw_typecode TYPECODE_TUPLE
-
-        ; Initialize slots to f.
-        mov     rax, f_value
-        popd    rcx                     ; length in rcx
-        mov     rdi, this_register
-        add     rdi, 16
-        rep     stosq
-
-        ; -- layout
-        _this_set_slot1                 ; --
-
-        pushrbx
-        mov     rbx, this_register      ; -- tuple
-
-        ; Return handle of allocated array.
-        _ new_handle                    ; -- handle
-
-        pop     this_register
-        next
-endcode
-
-; ### new
-code new, 'new'                         ; class-symbol -- tuple
-        _ new_tuple
         next
 endcode
 
@@ -215,35 +118,5 @@ code tuple_to_string, 'tuple>string'    ; tuple --
         _ sbuf_push
         _ sbuf_to_string
 
-        next
-endcode
-
-; ### .tuple
-code dot_tuple, '.tuple'                ; tuple --
-        _ check_tuple
-
-        push    this_register
-        popd    this_register
-
-        _write "T{ "
-
-        _this_slot 1                    ; -- layout
-        _dup
-        _ first
-        _ dot_object
-        _ space
-
-        _ second
-        _untag_fixnum
-        _register_do_times .1
-        _i
-        add     rbx, 2
-        _this_nth_slot
-        _ dot_object
-        _ space
-        _loop .1
-        _write "}"
-
-        pop     this_register
         next
 endcode
