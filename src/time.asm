035a5c98d0e8cd28b4d7f361229c2cbe9b867c16
Peter Graves <gnooth@gmail.com>
2018-02-23 11:24:35 -0800

    * src/time.asm (time): Improve formatting.

--- src/time.asm
+++ src/time.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2012-2017 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2012-2018 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -96,6 +96,8 @@ code time, 'time'                       ; callable --
         _ elapsed                       ; -- ns cycles
 
         _swap                           ; -- cycles ns
+
+        _ ?nl
 
         _ fixnum_to_float
         _lit tagged_fixnum(1000000)
