2364fbac28a8e2a3e600534c803f408c5168d0e1
Peter Graves <gnooth@gmail.com>
2018-03-09 10:24:35 -0800

    * src/vector.asm (vector-all?): New. * src/test.feline (vector-all?-test-1): New test.

--- src/vector.asm
+++ src/vector.asm
@@ -863,6 +863,46 @@ code vector_each, 'vector-each'         ; vector callable --
         next
 endcode
 
+; ### vector-all?
+code vector_all?, 'vector-all?'         ; vector callable -- ?
+
+        ; protect callable from gc
+        push    rbx
+
+        _ callable_raw_code_address     ; -- vector code-address
+
+        _swap
+        _ check_vector                  ; -- code-address vector
+
+        push    this_register
+        mov     this_register, rbx
+        push    r12
+        mov     r12, [rbp]              ; code address in r12
+        _2drop                          ; adjust stack
+
+        _t                              ; initialize return value
+
+        _this_vector_raw_length
+        _do_times .1
+        _raw_loop_index
+        _this_vector_nth_unsafe         ; -- t element
+        call    r12                     ; -- t ?
+        cmp     rbx, f_value
+        poprbx                          ; -- t
+        jne     .2
+        mov     rbx, f_value            ; -- f
+        _leave .1
+.2:
+        _loop .1
+        pop     r12
+        pop     this_register
+
+        ; drop callable
+        pop     rax
+
+        next
+endcode
+
 ; ### vector-each-index
 code vector_each_index, 'vector-each-index' ; vector quotation-or-xt --
         _ callable_raw_code_address     ; -- vector code-address
