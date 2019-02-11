d078ae6df6aedf716b1f29b1423cd2b80607efb1
Peter Graves <gnooth@gmail.com>
2019-02-10 18:01:25 -0800

    * src/sequences.asm (any?): Use _as_boolean.

--- src/sequences.asm
+++ src/sequences.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2016-2017 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2016-2019 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -811,10 +811,9 @@ endcode
 
 ; ### any?
 code any?, 'any?'                       ; seq quot -> ?
+; returns true if seq contains an element that satisfies the predicate
         _ find                          ; -> index/f element/f
         _nip
-        mov     rax, t_value
-        cmp     rbx, f_value
-        cmovne  rbx, rax
+        _as_boolean
         next
 endcode
