df68c3a0a7a7cb02fc50dc38814ceaffcb18d7fb
Peter Graves <gnooth@gmail.com>
2019-02-28 11:48:59 -0800

    * src/feline.asm: Remove version.asm.

--- src/feline.asm
+++ src/feline.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2012-2018 Peter Graves <gnooth@gmail.com>
+; Copyright (C) 2012-2019 Peter Graves <gnooth@gmail.com>
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
 default abs ; use absolute addresses by default
 
 %include "feline_home.asm"
-%include "version.asm"
 %include "externs.asm"
 %include "macros.asm"
 %include "loop-macros.asm"
