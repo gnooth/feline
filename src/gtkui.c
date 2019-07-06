8244f275a074c3f245a282843b6e912fb585fa95
Peter Graves <gnooth@gmail.com>
2019-07-05 17:36:24 -0700

    work in progress (mode line)

--- src/gtkui.c
+++ src/gtkui.c
@@ -15,20 +15,54 @@
 
 #include <gtk/gtk.h>
 
+static GtkWidget *window;
+
 static gboolean
 key_press_callback (GtkWidget *widget, GdkEventKey *event, gpointer data)
 {
   g_print ("key pressed 0x%08x 0x%08x %s\n", event->state, event->keyval,
            gdk_keyval_name (event->keyval));
+  if (event->keyval == 0x71)
+    {
+      gtk_widget_destroy (window);
+      gtk_main_quit ();
+    }
   return TRUE;
 }
 
 static gboolean
-draw_callback (GtkWidget *widget, cairo_t *cr, gpointer user_data)
+textview_draw_callback (GtkWidget *widget, cairo_t *cr, gpointer user_data)
 {
-  g_print ("draw_callback called\n");
+  g_print ("textview_draw_callback called\n");
   cairo_move_to (cr, 40, 40);
   cairo_set_font_size (cr, 16.0);
+  // black background
+  cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+  cairo_paint (cr);
+  // red text
+  cairo_set_source_rgb (cr, 1.0, 0.0, 0.0);
+  cairo_show_text (cr, "This is a test!");
+  return TRUE;
+}
+
+static gboolean
+modeline_draw_callback (GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  g_print ("modeline_draw_callback called\n");
+
+  cairo_text_extents_t extents;
+  cairo_text_extents (cr, "test", &extents);
+  double char_width = extents.width / 4;
+  double char_height = extents.height;
+  g_print ("char_width = %f char_height = %f\n", char_width, char_height);
+
+  cairo_move_to (cr, 0, 16);
+  cairo_set_font_size (cr, 16.0);
+  // white background
+  cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+  cairo_paint (cr);
+  // black text
+  cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
   cairo_show_text (cr, "This is a test!");
   return TRUE;
 }
@@ -36,16 +70,20 @@ draw_callback (GtkWidget *widget, cairo_t *cr, gpointer user_data)
 void gtkui__initialize (void)
 {
   gtk_init(0,  NULL);
-  GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title (GTK_WINDOW (window), "Feral");
-  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+  gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
 
-  GtkWidget *drawing_area = gtk_drawing_area_new();
-  gtk_widget_set_size_request (drawing_area, 400, 400);
-  gtk_container_add (GTK_CONTAINER (window), drawing_area);
-  gtk_widget_set_can_focus (drawing_area, TRUE);
+  GtkWidget *box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 1);
+  gtk_container_add (GTK_CONTAINER (window), box);
 
-  gtk_widget_set_events (drawing_area,
+  GtkWidget *drawing_area_1 = gtk_drawing_area_new();
+  gtk_widget_set_size_request (drawing_area_1, 800, 568);
+  gtk_container_add (GTK_CONTAINER (box), drawing_area_1);
+  gtk_widget_set_can_focus (drawing_area_1, TRUE);
+
+  gtk_widget_set_events (drawing_area_1,
                          //                          GDK_EXPOSURE_MASK |
                          //                          GDK_ENTER_NOTIFY_MASK |
                          //                          GDK_LEAVE_NOTIFY_MASK |
@@ -58,11 +96,36 @@ void gtkui__initialize (void)
                          //                          GDK_POINTER_MOTION_HINT_MASK
                          );
 
-  g_signal_connect (drawing_area, "draw", G_CALLBACK (draw_callback), NULL);
-  g_signal_connect (drawing_area, "key-press-event",
+  g_signal_connect (drawing_area_1, "draw",
+                    G_CALLBACK (textview_draw_callback), NULL);
+  g_signal_connect (drawing_area_1, "key-press-event",
                     G_CALLBACK(key_press_callback), NULL);
 
-  gtk_widget_show_all (window);
 
-//   gtk_main ();
+  GtkWidget *drawing_area_2 = gtk_drawing_area_new();
+  gtk_widget_set_size_request (drawing_area_2, 568, 32);
+  gtk_container_add (GTK_CONTAINER (box), drawing_area_2);
+  gtk_widget_set_can_focus (drawing_area_2, TRUE);
+
+  gtk_widget_set_events (drawing_area_2,
+                         //                          GDK_EXPOSURE_MASK |
+                         //                          GDK_ENTER_NOTIFY_MASK |
+                         //                          GDK_LEAVE_NOTIFY_MASK |
+                         //                          GDK_BUTTON_PRESS_MASK |
+                         //                          GDK_BUTTON_RELEASE_MASK |
+                         //                          GDK_SCROLL_MASK |
+                         GDK_KEY_PRESS_MASK
+                         //                          GDK_KEY_RELEASE_MASK |
+                         //                          GDK_POINTER_MOTION_MASK |
+                         //                          GDK_POINTER_MOTION_HINT_MASK
+                         );
+
+  g_signal_connect (drawing_area_2, "draw",
+                    G_CALLBACK (modeline_draw_callback), NULL);
+//   g_signal_connect (drawing_area_2, "key-press-event",
+//                     G_CALLBACK(key_press_callback), NULL);
+
+  gtk_widget_show_all (window);
+  g_print ("leaving gtkui__initialize\n");
+  gtk_main ();
 }
