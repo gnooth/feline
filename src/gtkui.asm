c3495a486e4bc98aea978b926737df4e2df1e531
Peter Graves <gnooth@gmail.com>
2019-09-01 10:12:11 -0700

    * src/gtkui.c: New file. * src/gtkui.asm: New file.

---
+++ src/gtkui.asm
@@ -0,0 +1,392 @@
+; Copyright (C) 2019 Peter Graves <gnooth@gmail.com>
+
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+file __FILE__
+
+asm_global gtkui_raw_sp0_, 0
+
+; ### gtkui-initialize
+code gtkui_initialize, 'gtkui-initialize'
+
+        _ current_thread_raw_sp0
+        mov     [gtkui_raw_sp0_], rbx
+        _drop
+
+        extern  gtkui__initialize
+        xcall   gtkui__initialize
+
+        next
+endcode
+
+; ### gtkui-main
+code gtkui_main, 'gtkui-main'           ; void -> void
+        extern  gtkui__main
+        xcall   gtkui__main
+        next
+endcode
+
+; ### gtkui_close
+subroutine gtkui_close                  ; void -> void
+; 0-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        _quote "do-quit"
+        _quote "editor"
+        _ ?lookup_symbol
+        _ call_symbol
+
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui-exit
+code gtkui_exit, 'gtkui-exit'
+        extern  gtkui__exit
+        xcall   gtkui__exit
+        next
+endcode
+
+; ### gtkui-textview-rows
+code gtkui_textview_rows, 'gtkui-textview-rows' ; void -> fixnum
+        extern  gtkui__textview_rows
+        xcall   gtkui__textview_rows
+        pushrbx
+        mov     rbx, rax
+        _tag_fixnum
+        next
+endcode
+
+; ### gtkui-textview-columns
+code gtkui_textview_columns, 'gtkui-textview-columns' ; void -> fixnum
+        extern  gtkui__textview_columns
+        xcall   gtkui__textview_columns
+        pushrbx
+        mov     rbx, rax
+        _tag_fixnum
+        next
+endcode
+
+; ### gtkui-textview-set-fg-color
+code gtkui_textview_set_fg_color, 'gtkui-textview-set-fg-color' ; color -> void
+        _check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+        extern  gtkui__textview_set_fg_color
+        xcall   gtkui__textview_set_fg_color
+        next
+endcode
+
+; ### gtkui-textview-set-bg-color
+code gtkui_textview_set_bg_color, 'gtkui-textview-set-bg-color' ; color -> void
+        _check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+        extern  gtkui__textview_set_bg_color
+        xcall   gtkui__textview_set_bg_color
+        next
+endcode
+
+; ### gtkui-char-width
+code gtkui_char_width, 'gtkui-char-width' ; void -> fixnum
+        extern  gtkui__char_width
+        xcall   gtkui__char_width
+        pushrbx
+        mov     rbx, rax
+        _tag_fixnum
+        next
+endcode
+
+; ### gtkui-char-height
+code gtkui_char_height, 'gtkui-char-height' ; void -> fixnum
+        extern  gtkui__char_height
+        xcall   gtkui__char_height
+        pushrbx
+        mov     rbx, rax
+        _tag_fixnum
+        next
+endcode
+
+; ### gtkui-modeline-set-text
+code gtkui_modeline_set_text, 'gtkui-modeline-set-text' ; string -> void
+        _ string_raw_data_address
+        mov     arg0_register, rbx
+        poprbx
+
+        extern  gtkui__modeline_set_text
+        xcall   gtkui__modeline_set_text
+
+        next
+endcode
+
+; ### gtkui-textview-text-out
+code gtkui_textview_text_out, 'gtkui-textview-text-out' ; x y string -> void
+        _ string_from
+        _drop
+        mov     arg2_register, rbx
+        poprbx
+        _ check_fixnum
+        mov     arg1_register, rbx
+        poprbx
+        _ check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+
+        extern  gtkui__textview_text_out
+        xcall   gtkui__textview_text_out
+
+        next
+endcode
+
+; ### gtkui-textview-clear-eol
+code gtkui_textview_clear_eol, 'gtkui-textview-clear-eol' ; x y -> void
+        _check_fixnum
+        mov     arg1_register, rbx
+        poprbx
+        _check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+
+        extern  gtkui__textview_clear_eol
+        xcall   gtkui__textview_clear_eol
+
+        next
+endcode
+
+; ### gtkui-textview-invalidate
+code gtkui_textview_invalidate, 'gtkui-textview-invalidate'
+
+        extern  gtkui__textview_invalidate
+        xcall   gtkui__textview_invalidate
+
+        next
+endcode
+
+; ### gtkui_textview_paint
+subroutine gtkui_textview_paint         ; void -> void
+; 0-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        _quote "repaint"
+        _quote "editor"
+        _ ?lookup_symbol
+        _ call_symbol
+
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui_textview_keydown
+subroutine gtkui_textview_keydown       ; keyval -> void
+; 1-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        pushrbx
+        mov     rbx, arg0_register
+        _tag_fixnum
+
+        _quote "gtkui-textview-keydown"
+        _quote "editor"
+        _ ?lookup_symbol
+        _ call_symbol
+
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui_textview_button_press
+subroutine gtkui_textview_button_press  ; x y -> void
+; 2-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        pushrbx
+        mov     rbx, arg0_register      ; x
+        _tag_fixnum
+        pushrbx
+        mov     rbx, arg1_register      ; y
+        _tag_fixnum
+
+        _quote "gtkui-textview-button-press"
+        _quote "editor"
+        _ ?lookup_symbol
+        _ call_symbol
+
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui_textview_mousewheel
+subroutine gtkui_textview_mousewheel    ; +1/-1 -> void
+; 1-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        test    arg0_register, arg0_register
+        js      .1
+
+        _quote "mousewheel-scroll-up"
+        _quote "editor"
+        _ ?lookup_symbol
+        _ call_symbol
+        jmp     .exit
+
+.1:
+        _quote "mousewheel-scroll-down"
+        _quote "editor"
+        _ ?lookup_symbol
+        _ call_symbol
+
+.exit:
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui-textview-set-caret-pos
+code gtkui_textview_set_caret_pos, 'gtkui-textview-set-caret-pos' ; x y -> void
+        _check_fixnum
+        mov     arg1_register, rbx
+        poprbx
+        _check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+        extern  gtkui__textview_set_caret_pos
+        xcall   gtkui__textview_set_caret_pos
+        next
+endcode
+
+; ### gtkui-minibuffer-main
+code gtkui_minibuffer_main, 'gtkui-minibuffer-main' ; void -> void
+        extern  gtkui__minibuffer_main
+        xcall   gtkui__minibuffer_main
+        next
+endcode
+
+; ### gtkui-minibuffer-exit
+code gtkui_minibuffer_exit, 'gtkui-minibuffer-exit' ; void -> void
+        extern  gtkui__minibuffer_exit
+        xcall   gtkui__minibuffer_exit
+        next
+endcode
+
+; ### gtkui-minibuffer-text-out
+code gtkui_minibuffer_text_out, 'gtkui-minibuffer-text-out' ; x y string -> void
+        _ string_from
+        _drop
+        mov     arg2_register, rbx
+        poprbx
+        _ check_fixnum
+        mov     arg1_register, rbx
+        poprbx
+        _ check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+
+        extern  gtkui__minibuffer_text_out
+        xcall   gtkui__minibuffer_text_out
+
+        next
+endcode
+
+; ### gtkui_minibuffer_paint
+subroutine gtkui_minibuffer_paint       ; void -> void
+; 0-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        _quote "repaint-minibuffer"     ; name
+        _quote "mini"                   ; vocab-name
+        _ ?lookup_symbol
+        _ call_symbol
+
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui_minibuffer_keydown
+subroutine gtkui_minibuffer_keydown     ; wparam -> void
+; 1-arg callback
+
+        ; enter callback
+        push    rbx
+        push    rbp
+        mov     rbp, [gtkui_raw_sp0_]
+
+        pushrbx
+        mov     rbx, arg0_register
+        _tag_fixnum
+
+        _quote "gtkui-minibuffer-keydown"
+        _quote "mini"
+        _ ?lookup_symbol
+        _ call_symbol
+
+        ; leave callback
+        pop     rbp
+        pop     rbx
+
+        ret
+endsub
+
+; ### gtkui-minibuffer-set-caret-pos
+code gtkui_minibuffer_set_caret_pos, 'gtkui-minibuffer-set-caret-pos' ; x y -> void
+        _check_fixnum
+        mov     arg1_register, rbx
+        poprbx
+        _check_fixnum
+        mov     arg0_register, rbx
+        poprbx
+        extern  gtkui__minibuffer_set_caret_pos
+        xcall   gtkui__minibuffer_set_caret_pos
+        next
+endcode
---
+++ src/gtkui.c
@@ -0,0 +1,583 @@
+// Copyright (C) 2019 Peter Graves <gnooth@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <gtk/gtk.h>
+
+#include "feline.h"
+
+#define FERAL_DEFAULT_FRAME_WIDTH       800
+#define FERAL_DEFAULT_FRAME_HEIGHT      600
+
+#define FERAL_DEFAULT_FONT_SIZE         14.0
+
+#define FERAL_DEFAULT_CHAR_HEIGHT       16
+
+#define FERAL_DEFAULT_MODELINE_HEIGHT   20
+#define FERAL_DEFAULT_MINIBUFFER_HEIGHT 20
+
+#define FERAL_DEFAULT_TEXTVIEW_HEIGHT \
+ (FERAL_DEFAULT_FRAME_HEIGHT - \
+  FERAL_DEFAULT_MODELINE_HEIGHT - \
+  FERAL_DEFAULT_MINIBUFFER_HEIGHT)
+
+static int char_width = 0;
+static int char_height = FERAL_DEFAULT_CHAR_HEIGHT;
+
+static int textview_rows
+  = FERAL_DEFAULT_FRAME_HEIGHT / FERAL_DEFAULT_CHAR_HEIGHT - 2;
+static int textview_columns = 0;
+
+static GtkWidget *frame;
+
+static GtkWidget *textview;
+static GtkWidget *modeline;
+static GtkWidget *minibuffer;
+
+static cairo_t *cr_textview;
+static cairo_t *cr_minibuffer;
+
+static COLORREF rgb_textview_fg = RGB (192, 192, 192);
+static COLORREF rgb_textview_bg = 0;
+
+static COLORREF rgb_modeline_fg = 0;
+static COLORREF rgb_modeline_bg = RGB (192, 192, 192);
+
+static COLORREF rgb_minibuffer_fg = RGB (192, 192, 192);
+static COLORREF rgb_minibuffer_bg = 0;
+
+double rgb_red (COLORREF rgb)
+{
+  return (rgb & 0xff) / 255.0;
+}
+
+double rgb_green (COLORREF rgb)
+{
+  return ((rgb >> 8) & 0xff) / 255.0;
+}
+
+double rgb_blue (COLORREF rgb)
+{
+  return ((rgb >> 16) & 0xff) / 255.0;
+}
+
+static const char *mode_line_text;
+
+static int textview_caret_row;
+static int textview_caret_column;
+
+static int minibuffer_caret_row;
+static int minibuffer_caret_column;
+
+static gboolean gtkui__textview_key_press (GtkWidget *widget,
+                                           GdkEventKey *event,
+                                           gpointer data);
+
+static gboolean gtkui__textview_mousewheel (GtkWidget *widget,
+                                            GdkEventScroll *event,
+                                            gpointer data);
+
+static gboolean gtkui__textview_button_press (GtkWidget *widget,
+                                              GdkEventButton *event,
+                                              gpointer data);
+
+static gboolean on_minibuffer_key_press (GtkWidget *widget,
+                                         GdkEventKey *event,
+                                         gpointer data);
+
+static gboolean on_modeline_draw (GtkWidget *widget,
+                                  cairo_t *cr,
+                                  gpointer data);
+
+static gboolean gtkui__minibuffer_draw (GtkWidget *widget,
+                                        cairo_t *cr,
+                                        gpointer data);
+
+static gboolean gtkui__textview_draw (GtkWidget *widget,
+                                      cairo_t *cr,
+                                      gpointer data);
+
+static gboolean gtkui__textview_size_allocate (GtkWidget *widget,
+                                               GdkRectangle* allocation,
+                                               gpointer data);
+
+static gboolean gtkui__frame_configure (GtkWidget *widget,
+                                        GdkEventConfigure *event,
+                                        gpointer data);
+
+// gtkui.asm
+extern void gtkui_close (void);
+
+static gboolean
+gtkui__close (GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+  gtkui_close ();
+  return TRUE;
+}
+
+void gtkui__initialize (void)
+{
+  gtk_init(0,  NULL);
+
+  frame = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title (GTK_WINDOW (frame), "Feral");
+  gtk_window_set_default_size (GTK_WINDOW (frame),
+                               FERAL_DEFAULT_FRAME_WIDTH,
+                               FERAL_DEFAULT_FRAME_HEIGHT);
+
+  g_signal_connect (frame, "configure-event",
+                    G_CALLBACK (gtkui__frame_configure), NULL);
+
+  g_signal_connect (frame, "delete-event", G_CALLBACK (gtkui__close), NULL);
+
+  GtkBox *box = GTK_BOX (gtk_box_new (GTK_ORIENTATION_VERTICAL, 0));
+  gtk_container_add (GTK_CONTAINER (frame), GTK_WIDGET(box));
+
+  textview = gtk_drawing_area_new();
+
+  gtk_widget_set_size_request (textview, -1, -1);
+
+  gtk_widget_set_can_focus (textview, TRUE);
+
+  gtk_widget_set_events (textview,
+                         GDK_BUTTON_PRESS_MASK |
+                         GDK_SCROLL_MASK |
+                         GDK_KEY_PRESS_MASK
+                         );
+
+  g_signal_connect (textview, "draw",
+                    G_CALLBACK (gtkui__textview_draw), NULL);
+  g_signal_connect (textview, "key-press-event",
+                    G_CALLBACK(gtkui__textview_key_press), NULL);
+  g_signal_connect (textview, "button-press-event",
+                    G_CALLBACK(gtkui__textview_button_press), NULL);
+  g_signal_connect (textview, "scroll-event",
+                    G_CALLBACK(gtkui__textview_mousewheel), NULL);
+  g_signal_connect (textview, "size-allocate",
+                    G_CALLBACK (gtkui__textview_size_allocate), NULL);
+
+  modeline = gtk_drawing_area_new();
+
+  gtk_widget_set_size_request (modeline,
+                               -1,
+                               FERAL_DEFAULT_MODELINE_HEIGHT);
+
+  gtk_widget_set_can_focus (modeline, TRUE);
+
+  gtk_widget_set_events (modeline, GDK_KEY_PRESS_MASK);
+
+  g_signal_connect (modeline, "draw",
+                    G_CALLBACK (on_modeline_draw), NULL);
+
+  minibuffer = gtk_drawing_area_new();
+
+  gtk_widget_set_size_request (minibuffer,
+                               -1,
+                               FERAL_DEFAULT_MINIBUFFER_HEIGHT);
+
+  gtk_widget_set_can_focus (minibuffer, TRUE);
+
+  gtk_widget_set_events (minibuffer,
+                         GDK_BUTTON_PRESS_MASK | GDK_KEY_PRESS_MASK );
+
+  g_signal_connect (minibuffer, "draw",
+                    G_CALLBACK (gtkui__minibuffer_draw), NULL);
+
+  g_signal_connect (minibuffer, "key-press-event",
+                    G_CALLBACK (on_minibuffer_key_press), NULL);
+
+  gtk_box_pack_end (box, minibuffer, FALSE, FALSE, 0);
+  gtk_box_pack_end (box, modeline, FALSE, FALSE, 0);
+  gtk_box_pack_end (box, textview, TRUE, TRUE, 0);
+
+  // REVIEW
+  gtk_window_move (GTK_WINDOW (frame), 480, 0);
+
+  gtk_widget_show_all (frame);
+}
+
+void gtkui__textview_invalidate (void)
+{
+  gtk_widget_queue_draw (textview);
+  gtk_widget_queue_draw (modeline);
+}
+
+static gboolean gtkui__textview_size_allocate (GtkWidget *widget,
+                                               GdkRectangle *allocation,
+                                               gpointer data)
+{
+  if (char_height)
+    textview_rows = allocation->height / char_height;
+  if (char_width)
+    textview_columns = allocation->width / char_width;
+  return TRUE;
+}
+
+static gboolean gtkui__frame_configure (GtkWidget *widget,
+                                        GdkEventConfigure *event,
+                                        gpointer data)
+{
+  gtk_widget_queue_resize (textview);
+  return TRUE;
+}
+
+void gtkui__main (void)
+{
+  gtk_main ();
+}
+
+void gtkui__exit (void)
+{
+  gtk_widget_destroy (frame);
+  gtk_main_quit ();
+}
+
+int gtkui__textview_rows (void)
+{
+  return textview_rows;
+}
+
+int gtkui__textview_columns (void)
+{
+  return textview_columns;
+}
+
+void gtkui__textview_set_fg_color (int rgb)
+{
+  rgb_textview_fg = rgb;
+}
+
+void gtkui__textview_set_bg_color (int rgb)
+{
+  rgb_textview_bg = rgb;
+}
+
+int gtkui__char_height (void)
+{
+  return char_height;
+}
+
+int gtkui__char_width (void)
+{
+  return char_width;
+}
+
+// gtkui.asm
+extern void gtkui_textview_keydown (guint);
+extern void gtkui_textview_button_press (int, int);
+
+#define ALT_MASK        0x01 << 16
+#define CTRL_MASK       0x02 << 16
+#define SHIFT_MASK      0x04 << 16
+
+void gtkui__modeline_set_text (const char *s)
+{
+  mode_line_text = s;
+}
+
+static void gtkui__textview_keydown (GdkEventKey *event)
+{
+  guint keyval = event->keyval;
+  guint state = event->state;
+  if (state & GDK_MOD1_MASK)
+    keyval |= ALT_MASK;
+  if (state & GDK_CONTROL_MASK)
+    keyval |= CTRL_MASK;
+  if (state & GDK_SHIFT_MASK)
+    keyval |= SHIFT_MASK;
+  gtkui_textview_keydown (keyval);
+  gtk_widget_queue_draw (frame);
+}
+
+static gboolean
+gtkui__textview_button_press (GtkWidget *widget,
+                              GdkEventButton *event,
+                              gpointer data)
+{
+  gtkui_textview_button_press (event->x, event->y);
+  return TRUE;
+}
+
+extern void gtkui_textview_mousewheel (cell);
+
+static gboolean
+gtkui__textview_mousewheel (GtkWidget *widget,
+                            GdkEventScroll *event,
+                            gpointer data)
+{
+  if (event->direction == GDK_SCROLL_UP)
+    gtkui_textview_mousewheel (1);
+  else if (event->direction == GDK_SCROLL_DOWN)
+    gtkui_textview_mousewheel (-1);
+  gtkui__textview_invalidate ();
+  return TRUE;
+}
+
+static gboolean
+gtkui__textview_key_press (GtkWidget *widget,
+                           GdkEventKey *event,
+                           gpointer data)
+{
+  if (event->keyval == GDK_KEY_Control_L || event->keyval == GDK_KEY_Control_R)
+    return FALSE;
+  if (event->keyval == GDK_KEY_Alt_L || event->keyval == GDK_KEY_Alt_R)
+    return FALSE;
+  if (event->keyval == GDK_KEY_Shift_L || event->keyval == GDK_KEY_Shift_R)
+    return FALSE;
+  gtkui__textview_keydown (event);
+  return TRUE;
+}
+
+extern void gtkui_minibuffer_keydown (guint);
+
+static gboolean
+on_minibuffer_key_press (GtkWidget *widget, GdkEventKey *event, gpointer data)
+{
+  if (event->keyval == GDK_KEY_Control_L || event->keyval == GDK_KEY_Control_R)
+    return FALSE;
+  if (event->keyval == GDK_KEY_Alt_L || event->keyval == GDK_KEY_Alt_R)
+    return FALSE;
+
+  guint keyval = event->keyval;
+
+  guint state = event->state;
+  if (state & GDK_MOD1_MASK)
+    keyval |= ALT_MASK;
+  if (state & GDK_CONTROL_MASK)
+    keyval |= CTRL_MASK;
+  if (state & GDK_SHIFT_MASK)
+    keyval |= SHIFT_MASK;
+
+  gtkui_minibuffer_keydown (keyval);
+  gtk_widget_queue_draw (minibuffer);
+
+  return TRUE;
+}
+
+extern void gtkui_textview_paint (void);
+
+void gtkui__textview_text_out (int x, int y, const char* s)
+{
+  if (cr_textview)
+    {
+      if (rgb_textview_bg != 0) // REVIEW
+        {
+          cairo_save (cr_textview);
+          cairo_set_source_rgb (cr_textview,
+                                rgb_red (rgb_textview_bg),
+                                rgb_green (rgb_textview_bg),
+                                rgb_blue (rgb_textview_bg));
+          cairo_rectangle (cr_textview,
+                           x,
+                           y - char_height + 3, // REVIEW
+                           char_width * strlen (s),
+                           char_height);
+          cairo_clip (cr_textview);
+          cairo_paint (cr_textview);
+          cairo_restore (cr_textview);
+        }
+
+      cairo_set_source_rgb (cr_textview,
+                            rgb_red (rgb_textview_fg),
+                            rgb_green (rgb_textview_fg),
+                            rgb_blue (rgb_textview_fg));
+      cairo_move_to (cr_textview, x, y);
+      cairo_show_text (cr_textview, s);
+    }
+  else
+    {
+      g_print ("no cr\n");
+    }
+}
+
+void gtkui__textview_clear_eol (int column, int row)
+{
+  cairo_save (cr_textview);
+  cairo_set_source_rgb (cr_textview,
+                        rgb_red (rgb_textview_bg),
+                        rgb_green (rgb_textview_bg),
+                        rgb_blue (rgb_textview_bg));
+  cairo_rectangle (cr_textview,
+                   column * char_width,
+                   row * char_height + 3, // REVIEW
+                   textview_columns * char_width,
+                   char_height);
+  cairo_clip (cr_textview);
+  cairo_paint (cr_textview);
+  cairo_restore (cr_textview);
+}
+
+static void
+gtkui__textview_draw_caret ()
+{
+  if (gtk_widget_is_focus (textview))
+    {
+      int x = char_width * textview_caret_column;
+      int y = char_height * textview_caret_row;
+      cairo_set_source_rgb (cr_textview, 1.0, 1.0, 1.0); // white
+      cairo_rectangle (cr_textview, x, y + 3, 2, char_height);
+      cairo_fill (cr_textview);
+    }
+}
+
+static gboolean
+gtkui__textview_draw (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+  g_return_val_if_fail (widget == textview, FALSE);
+
+  cr_textview = cr;
+
+  cairo_select_font_face (cr, "monospace",
+                          CAIRO_FONT_SLANT_NORMAL,
+                          CAIRO_FONT_WEIGHT_NORMAL);
+  cairo_set_font_size (cr, FERAL_DEFAULT_FONT_SIZE);
+
+  if (char_width == 0)
+    {
+      cairo_font_extents_t fe;
+      cairo_font_extents (cr, &fe);
+
+      char_width = (int) fe.max_x_advance;
+      char_height = (int) fe.height;
+
+      GtkAllocation allocation;
+      gtk_widget_get_allocation (widget, &allocation);
+
+      textview_rows = allocation.height / char_height;
+      textview_columns = allocation.width / char_width;
+    }
+
+  // black background
+  cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+  cairo_paint (cr);
+
+  // white text
+  cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+
+  gtkui_textview_paint ();
+
+  gtkui__textview_draw_caret ();
+
+  cr_textview = 0;
+  return TRUE;
+}
+
+static gboolean
+on_modeline_draw (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+  cairo_select_font_face (cr, "monospace",
+                          CAIRO_FONT_SLANT_NORMAL,
+                          CAIRO_FONT_WEIGHT_NORMAL);
+
+  cairo_move_to (cr, 0, 14);
+  cairo_set_font_size (cr, 14.0);
+  // white background
+  cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+  cairo_paint (cr);
+  // black text
+  cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+  cairo_show_text (cr, mode_line_text);
+  return TRUE;
+}
+
+static void
+gtkui__minibuffer_draw_caret (void)
+{
+  if (gtk_widget_is_focus (minibuffer))
+    {
+      cairo_set_source_rgb (cr_minibuffer, 1.0, 1.0, 1.0); // white
+
+      cairo_rectangle (cr_minibuffer,
+                       minibuffer_caret_column * char_width,
+                       3,
+                       2,
+                       char_height);
+      cairo_fill (cr_minibuffer);
+    }
+}
+
+extern void gtkui_minibuffer_paint (void);
+
+static gboolean
+gtkui__minibuffer_draw (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+  cr_minibuffer = cr;
+
+  GtkAllocation allocation;
+  gtk_widget_get_allocation (widget, &allocation);
+
+  cairo_select_font_face (cr, "monospace",
+                          CAIRO_FONT_SLANT_NORMAL,
+                          CAIRO_FONT_WEIGHT_NORMAL);
+
+  cairo_text_extents_t extents;
+  cairo_text_extents (cr, "test", &extents);
+  cairo_set_font_size (cr, 14.0);
+
+  // black background
+  cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+  cairo_paint (cr);
+  // white text
+  cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+  gtkui_minibuffer_paint ();
+
+  gtkui__minibuffer_draw_caret ();
+
+  cr_minibuffer = 0;
+
+  return TRUE;
+}
+
+void gtkui__textview_set_caret_pos (int column, int row)
+{
+  textview_caret_column = column;
+  textview_caret_row = row;
+}
+
+void gtkui__minibuffer_main (void)
+{
+  gtk_widget_queue_draw (minibuffer);
+  gtk_widget_grab_focus (minibuffer);
+
+  // nested call to gtk_main
+  gtk_main ();
+
+  gtk_widget_grab_focus (textview);
+}
+
+void gtkui__minibuffer_exit (void)
+{
+  // return from nested call to gtk_main
+  gtk_main_quit();
+}
+
+void gtkui__minibuffer_text_out (int x, int y, const char* s)
+{
+  if (*s == 0) // empty string
+    return;
+
+  if (cr_minibuffer)
+    {
+      cairo_move_to (cr_minibuffer, x, y);
+      cairo_show_text (cr_minibuffer, s);
+    }
+  else
+    g_print ("gtkui__minibuffer_text_out no cr\n");
+}
+
+void gtkui__minibuffer_set_caret_pos (int column, int row)
+{
+  minibuffer_caret_column = column;
+  minibuffer_caret_row = row;
+}
